0997d9a758e8da2185fb8b37c8e20e7548303f8f
Łukasz <lukasz.malucha1@gmail.com>
2023-03-17 14:48:41 +0100

    Add Turborepo

---
+++ apps/client/src/components/UI/Input/Input.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import styles from "./Input.module.css";
+import clsx from "clsx";
+
+interface InputProps {
+  className?: string;
+  placeholder: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+  isError?: boolean;
+  id?: string;
+  type?: string;
+}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, placeholder, onChange, value, isError, id, type }, ref) => {
+    return (
+      <input
+        id={id}
+        type={type || "text"}
+        placeholder={placeholder}
+        onChange={onChange}
+        className={clsx(
+          `${styles.input}`,
+          `${className}`,
+          isError && `${styles.error}`
+        )}
+        value={value}
+        ref={ref}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
---
+++ apps/client/src/components/UI/Sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+import styles from "./Sidebar.module.css";
+
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+export const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  return <aside className={styles.sidebar}>{children}</aside>;
+};
