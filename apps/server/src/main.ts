fec0693244fc2cf102ebd289dbcb264656b39b98
LukaszzM <lukaszzmalucha@gmail.com>
2023-08-15 22:43:03 +0200

    Add base swagger

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
@@ -9,6 +10,16 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle("Chatify - API")
+    .setDescription("Real-time chat application")
+    .setVersion("1.0")
+    .addTag("chat")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
   await app.listen(3000);
 }
 bootstrap();
