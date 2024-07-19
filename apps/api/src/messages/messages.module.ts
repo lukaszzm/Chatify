655cc4181238d63479d2e1d40940a2cf9d1b5d21
lukaszzm <lukasz.malucha1@gmail.com>
2024-07-19 14:03:59 +0200

    feat(api): add messages resolver

--- apps/api/src/messages/messages.module.ts
+++ apps/api/src/messages/messages.module.ts
@@ -1,10 +1,18 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "nestjs-prisma";
 
+import { ChatsService } from "@/chats/chats.service";
 import { MessagesResolver } from "@/messages/messages.resolver";
 import { MessagesService } from "@/messages/messages.service";
+import { UsersService } from "@/users/users.service";
 
 @Module({
-  providers: [MessagesService, MessagesResolver, PrismaService],
+  providers: [
+    MessagesService,
+    MessagesResolver,
+    PrismaService,
+    UsersService,
+    ChatsService,
+  ],
 })
 export class MessagesModule {}
--- apps/api/src/messages/messages.service.ts
+++ apps/api/src/messages/messages.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "nestjs-prisma";
 
+import { SendMessageInput } from "@/messages/dtos/send-message.input";
+
 @Injectable()
 export class MessagesService {
   constructor(private readonly prismaService: PrismaService) {}
@@ -9,6 +11,24 @@ export class MessagesService {
     return this.prismaService.message.findMany({
       where: {
         chatId,
+      },
+    });
+  }
+
+  async create(data: SendMessageInput, senderId: string) {
+    return this.prismaService.message.create({
+      data: {
+        content: data.content,
+        chat: {
+          connect: {
+            id: data.chatId,
+          },
+        },
+        sender: {
+          connect: {
+            id: senderId,
+          },
+        },
       },
     });
   }
