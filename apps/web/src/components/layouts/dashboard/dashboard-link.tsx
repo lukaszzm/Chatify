4f4dda126fa5db8c2757314521c2965b4a0ab801
lukaszzm <lukasz.malucha1@gmail.com>
2024-07-24 15:52:13 +0200

    fix(web): active dashboard link styles

--- apps/web/src/components/layouts/dashboard/dashboard-link.tsx
+++ apps/web/src/components/layouts/dashboard/dashboard-link.tsx
@@ -1,6 +1,6 @@
 import { TooltipButton } from "@chatify/ui";
 import type { LinkProps } from "@tanstack/react-router";
-import { Link } from "@tanstack/react-router";
+import { Link, useLocation } from "@tanstack/react-router";
 
 interface DashboardLinkProps {
   label: string;
@@ -9,16 +9,12 @@ interface DashboardLinkProps {
 }
 
 export const DashboardLink = ({ children, label, to }: DashboardLinkProps) => {
+  const location = useLocation();
+  const isActive = location.pathname === to;
+
   return (
-    <TooltipButton tooltipText={label} asChild>
-      <Link
-        to={to}
-        activeProps={{
-          className: "text-accent/90 bg-accent/5 hover:text-accent",
-        }}
-      >
-        {children}
-      </Link>
+    <TooltipButton tooltipText={label} asChild variant={isActive ? "active" : "nav"}>
+      <Link to={to}>{children}</Link>
     </TooltipButton>
   );
 };
