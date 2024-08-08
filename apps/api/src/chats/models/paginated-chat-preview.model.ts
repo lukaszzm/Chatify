4b58622c6b7f29e5c89cc94d50a2c965529a68d4
lukaszzm <lukasz.malucha1@gmail.com>
2024-08-08 15:44:13 +0200

    feat(api): add default value for count items in pagination args

--- apps/api/src/chats/models/paginated-chat-preview.model.ts
+++ apps/api/src/chats/models/paginated-chat-preview.model.ts
@@ -3,5 +3,5 @@ import { ObjectType } from "@nestjs/graphql";
 import { ChatPreview } from "@/chats/models/chat-preview.model";
 import { Paginated } from "@/common/models/paginated.model";
 
-@ObjectType()
+@ObjectType("ChatPreviewConnection")
 export class PaginatedChatPreview extends Paginated(ChatPreview) {}
