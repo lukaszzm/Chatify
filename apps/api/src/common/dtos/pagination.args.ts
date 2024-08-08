a4207ba62fc40713b88d6ffdb6fbec11b4692e67
lukaszzm <lukasz.malucha1@gmail.com>
2024-08-08 15:44:13 +0200

    feat(api): add default value for count items in pagination args

--- apps/api/src/common/dtos/pagination.args.ts
+++ apps/api/src/common/dtos/pagination.args.ts
@@ -2,8 +2,8 @@ import { ArgsType, Field, Int } from "@nestjs/graphql";
 
 @ArgsType()
 export class PaginationArgs {
-  @Field(() => Int, { nullable: true })
-  first?: number;
+  @Field(() => Int, { defaultValue: 10 })
+  first: number;
 
   @Field(() => String, { nullable: true })
   after?: string;
