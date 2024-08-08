3bdc247a18665f617f5126a1c2a5086eb4a9babd
lukaszzm <lukasz.malucha1@gmail.com>
2024-08-08 15:11:13 +0200

    fix(api): extend paginated object model

--- apps/api/src/common/models/paginated.model.ts
+++ apps/api/src/common/models/paginated.model.ts
@@ -4,10 +4,19 @@ import { Field, ObjectType } from "@nestjs/graphql";
 import { PageInfo } from "@/common/models/page-info.model";
 
 export function Paginated<T>(classRef: Type<T>) {
+  @ObjectType(`${classRef.name}Edge`, { isAbstract: true })
+  abstract class EdgeType {
+    @Field(() => String)
+    cursor: string;
+
+    @Field(() => classRef)
+    node: T;
+  }
+
   @ObjectType({ isAbstract: true })
   abstract class PaginatedType {
-    @Field(() => [classRef])
-    edges: T[];
+    @Field(() => [EdgeType])
+    edges: EdgeType[];
 
     @Field(() => PageInfo)
     pageInfo: PageInfo;
