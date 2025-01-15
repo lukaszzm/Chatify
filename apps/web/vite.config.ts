ca0780aab96941d790181aa59446f23d193797b6
lukaszzm <lukasz.malucha1@gmail.com>
2025-01-15 10:28:45 +0100

    feat(web): add automatic code splitting

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [TanStackRouterVite(), react()],
   build: {
     chunkSizeWarningLimit: 750,
   },
