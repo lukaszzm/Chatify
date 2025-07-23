94cf5e7735d77d08ef995063bd0334191bfb119c
lukaszzm <lukasz.malucha1@gmail.com>
2025-07-23 14:52:09 +0200

    fix(web): sync times with UTC format

--- apps/web/src/utils/format-date.ts
+++ apps/web/src/utils/format-date.ts
@@ -1,18 +1,19 @@
 import { formatDistance, isSameWeek, formatDate as format, isSameDay } from "date-fns";
 
 export function formatDate(timestamp: string): string {
-  const now = new Date();
+  const now = new Date().toISOString();
+  const time = new Date(timestamp).toISOString();
 
-  if (isSameDay(timestamp, now)) {
-    return formatDistance(timestamp, now, { addSuffix: true }).replace(
+  if (isSameDay(time, now)) {
+    return formatDistance(time, now, { addSuffix: true }).replace(
       "less than a minute ago",
       "just now"
     );
   }
 
-  if (isSameWeek(timestamp, now)) {
-    return format(timestamp, "cccc	kk:mm");
+  if (isSameWeek(time, now)) {
+    return format(time, "cccc	kk:mm");
   }
 
-  return format(timestamp, "dd MMM yy, kk:mm");
+  return format(time, "dd MMM yy, kk:mm");
 }
